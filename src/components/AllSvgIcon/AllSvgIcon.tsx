0838008027c3092be81a7051fda061659f48335d
trinhviettoan1999 <trinhviettoan1999@gmail.com>
2021-06-01 12:16:17 +0700

    fix icon

--- src/components/AllSvgIcon/AllSvgIcon.tsx
+++ src/components/AllSvgIcon/AllSvgIcon.tsx
@@ -50,7 +50,7 @@ export const Back = () => (
   <SvgXml
     xml={`
     <svg width="28" height="28" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M14 6L8 12L14 18" stroke="#6A1616" stroke-width="2" stroke-linecap="round"/>
+    <path d="M14 6L8 12L14 18" stroke="#212121" stroke-width="2" stroke-linecap="round"/>
     </svg>  
     `}
   />
