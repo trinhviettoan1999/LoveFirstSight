4d5cf0e0eac8f1f2fba75515232fc63eab4ef4c8
trinhviettoan1999 <trinhviettoan1999@gmail.com>
2021-06-03 20:38:06 +0700

    change color primary and background default

--- src/theme/color.ts
+++ src/theme/color.ts
@@ -1,6 +1,7 @@
 export const color = {
   transparent: 'rgba(0,0,0,0)',
-  primary: '#6A1616',
+  primary: '#E0694E',
+  light: '#FCF0ED',
   second: '#F8F8F8',
   bgWhite: '#FFFFFF',
   bgWhiteLight: '#F1F1F1',
