fd10e288368f67a2ec240c0a6be3bbc7631988eb
trinhviettoan1999 <trinhviettoan1999@gmail.com>
2021-06-11 22:39:25 +0700

    edit stapler screen

--- src/screens/StaplerScreen/staplerScreen.tsx
+++ src/screens/StaplerScreen/staplerScreen.tsx
@@ -137,9 +137,9 @@ export const StaplerScreen = () => {
     await getAvailableUsers(filter).then(async (result) => {
       if (isMounted) {
         setUser(getUserRandom(result));
+        setLoad(false);
       }
     });
-    setLoad(false);
   }
 
   const handleYesAlert = () => {
@@ -200,159 +200,181 @@ export const StaplerScreen = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [load]);
 
-  return !user?.name && !load ? (
-    <View style={{justifyContent: 'center', alignItems: 'center', flex: 1}}>
-      <Image
-        source={not_result_image}
-        style={styles.imageResult}
-        resizeMode="contain"
-      />
-      <Text style={{fontSize: 16}}>Don't have Users with your filter</Text>
-    </View>
-  ) : (
-    <View>
-      <ScrollView
-        style={{width: WIDTH, height: HEIGHT - tabBarHeight}}
-        contentContainerStyle={{paddingBottom: showInfo ? 70 : 0}}>
-        <ImageBackground
-          source={{uri: user.avatar || undefined}}
-          style={styles.containerAll}
-          resizeMode="cover">
-          <HeaderCustom
-            backgroundStatusBar={color.transparent}
-            removeBorderWidth
-            barStyle="light-content"
-          />
-          <LinearGradient
-            colors={['rgba(255, 255, 255, 0)', '#000000']}
-            locations={[0.5323, 0.993]}
-            style={styles.image}>
-            <View style={{flexDirection: 'row'}}>
-              <Text style={styles.name}>
-                {`${user.name}  ${computeAge(user.birthday)}`}
-              </Text>
-              <TouchableOpacity onPress={() => setShowInfo(!showInfo)}>
-                <Info />
-              </TouchableOpacity>
-            </View>
-            <Hobbies data={user.hobbies} />
-          </LinearGradient>
-        </ImageBackground>
-        {showInfo && (
-          <View>
-            <View style={styles.infoContainer}>
-              <ProfileInformation iconName="gender" content={user.gender} />
-              {user.lookingFor ? (
-                <ProfileInformation
-                  iconName="lookingfor"
-                  content={user.lookingFor}
-                />
-              ) : null}
-              <ProfileInformation
-                iconName="location"
-                content="Located in Ho Chi Minh City, Viet Nam"
+  return (
+    <View style={styles.wrap}>
+      {load && (
+        <View style={styles.load}>
+          <ActivityIndicator color={color.primary} size="large" />
+          <Text style={styles.textLoad}>Loading...</Text>
+        </View>
+      )}
+      {user?.name && !load ? (
+        <View>
+          <ScrollView
+            style={{width: WIDTH, height: HEIGHT - tabBarHeight}}
+            contentContainerStyle={{paddingBottom: showInfo ? 70 : 0}}>
+            <ImageBackground
+              source={{uri: user.avatar || undefined}}
+              style={styles.containerAll}
+              resizeMode="cover">
+              <HeaderCustom
+                backgroundStatusBar={color.transparent}
+                removeBorderWidth
+                barStyle="light-content"
               />
-              <ProfileInformation
-                iconName="scope"
-                content={
-                  `${calculateDistance(user.coordinates, coordinate)
-                    .toFixed(1)
-                    .toString()}` + ' km'
-                }
-              />
-              {user.height ? (
-                <ProfileInformation
-                  iconName="height"
-                  content={user.height + ' cm'}
-                />
-              ) : null}
-              {user.university ? (
-                <ProfileInformation
-                  iconName="university"
-                  content={user.university}
-                />
-              ) : null}
-              {user.province ? (
-                <ProfileInformation
-                  iconName="province"
-                  content={user.province}
-                />
-              ) : null}
-              {user.drinking ? (
-                <ProfileInformation
-                  iconName="drinking"
-                  content={user.drinking}
-                />
-              ) : null}
-              {user.smoking ? (
-                <ProfileInformation iconName="smoking" content={user.smoking} />
-              ) : null}
-              {user.kids ? (
-                <ProfileInformation iconName="child" content={user.kids} />
-              ) : null}
-            </View>
-            <View style={{paddingHorizontal: spacing[4]}}>
-              {user.images[0] ? (
-                <ImageUser urlImage={user.images[0] || ''} />
-              ) : null}
-              {user.images[1] ? (
-                <ImageUser urlImage={user.images[1] || ''} />
-              ) : null}
-              {user.images[2] ? (
-                <ImageUser urlImage={user.images[2] || ''} />
-              ) : null}
-              {user.images[3] ? (
-                <ImageUser urlImage={user.images[3] || ''} />
-              ) : null}
-              {user.images[4] ? (
-                <ImageUser urlImage={user.images[4] || ''} />
-              ) : null}
-              {user.images[5] ? (
-                <ImageUser urlImage={user.images[5] || ''} />
-              ) : null}
-              {user.images[6] ? (
-                <ImageUser urlImage={user.images[6] || ''} />
-              ) : null}
-              {user.images[7] ? (
-                <ImageUser urlImage={user.images[7] || ''} />
-              ) : null}
-            </View>
+              <LinearGradient
+                colors={['rgba(255, 255, 255, 0)', '#000000']}
+                locations={[0.5323, 0.993]}
+                style={styles.image}>
+                <View style={{flexDirection: 'row'}}>
+                  <Text style={styles.name}>
+                    {`${user.name}  ${computeAge(user.birthday)}`}
+                  </Text>
+                  <TouchableOpacity onPress={() => setShowInfo(!showInfo)}>
+                    <Info />
+                  </TouchableOpacity>
+                </View>
+                <Hobbies data={user.hobbies} />
+              </LinearGradient>
+            </ImageBackground>
+            {showInfo && (
+              <View>
+                <View style={styles.infoContainer}>
+                  <ProfileInformation iconName="gender" content={user.gender} />
+                  {user.lookingFor ? (
+                    <ProfileInformation
+                      iconName="lookingfor"
+                      content={user.lookingFor}
+                    />
+                  ) : null}
+                  <ProfileInformation
+                    iconName="location"
+                    content="Located in Ho Chi Minh City, Viet Nam"
+                  />
+                  <ProfileInformation
+                    iconName="scope"
+                    content={
+                      `${calculateDistance(user.coordinates, coordinate)
+                        .toFixed(1)
+                        .toString()}` + ' km'
+                    }
+                  />
+                  {user.height ? (
+                    <ProfileInformation
+                      iconName="height"
+                      content={user.height + ' cm'}
+                    />
+                  ) : null}
+                  {user.university ? (
+                    <ProfileInformation
+                      iconName="university"
+                      content={user.university}
+                    />
+                  ) : null}
+                  {user.province ? (
+                    <ProfileInformation
+                      iconName="province"
+                      content={user.province}
+                    />
+                  ) : null}
+                  {user.drinking ? (
+                    <ProfileInformation
+                      iconName="drinking"
+                      content={user.drinking}
+                    />
+                  ) : null}
+                  {user.smoking ? (
+                    <ProfileInformation
+                      iconName="smoking"
+                      content={user.smoking}
+                    />
+                  ) : null}
+                  {user.kids ? (
+                    <ProfileInformation iconName="child" content={user.kids} />
+                  ) : null}
+                </View>
+                <View style={{paddingHorizontal: spacing[4]}}>
+                  {user.images[0] ? (
+                    <ImageUser urlImage={user.images[0] || ''} />
+                  ) : null}
+                  {user.images[1] ? (
+                    <ImageUser urlImage={user.images[1] || ''} />
+                  ) : null}
+                  {user.images[2] ? (
+                    <ImageUser urlImage={user.images[2] || ''} />
+                  ) : null}
+                  {user.images[3] ? (
+                    <ImageUser urlImage={user.images[3] || ''} />
+                  ) : null}
+                  {user.images[4] ? (
+                    <ImageUser urlImage={user.images[4] || ''} />
+                  ) : null}
+                  {user.images[5] ? (
+                    <ImageUser urlImage={user.images[5] || ''} />
+                  ) : null}
+                  {user.images[6] ? (
+                    <ImageUser urlImage={user.images[6] || ''} />
+                  ) : null}
+                  {user.images[7] ? (
+                    <ImageUser urlImage={user.images[7] || ''} />
+                  ) : null}
+                </View>
+              </View>
+            )}
+          </ScrollView>
+          <View style={styles.containerButton}>
+            <TouchableOpacity
+              activeOpacity={0.6}
+              onPress={async () => {
+                await ignoreUser(user.userId);
+                setLoad(!load);
+              }}>
+              <DisLike />
+            </TouchableOpacity>
+            <TouchableOpacity
+              activeOpacity={0.6}
+              onPress={async () => {
+                await superLikeUser(user.userId);
+                setLoad(!load);
+              }}>
+              <Star />
+            </TouchableOpacity>
+            <TouchableOpacity
+              activeOpacity={0.6}
+              onPress={async () => {
+                await likeUser(user.userId);
+                // @ts-ignore: Object is possibly 'null'.
+                sendNotification(user.userId, auth().currentUser?.uid);
+                setLoad(!load);
+              }}>
+              <Like />
+            </TouchableOpacity>
           </View>
-        )}
-      </ScrollView>
-      <View style={styles.containerButton}>
-        <TouchableOpacity
-          activeOpacity={0.6}
-          onPress={async () => {
-            await ignoreUser(user.userId);
-            setLoad(!load);
-          }}>
-          <DisLike />
-        </TouchableOpacity>
-        <TouchableOpacity
-          activeOpacity={0.6}
-          onPress={async () => {
-            await superLikeUser(user.userId);
-            setLoad(!load);
-          }}>
-          <Star />
-        </TouchableOpacity>
-        <TouchableOpacity
-          activeOpacity={0.6}
-          onPress={async () => {
-            await likeUser(user.userId);
-            // @ts-ignore: Object is possibly 'null'.
-            sendNotification(user.userId, auth().currentUser?.uid);
-            setLoad(!load);
-          }}>
-          <Like />
-        </TouchableOpacity>
-      </View>
+        </View>
+      ) : null}
+      {/* {!user?.name && !load && (
+        <View
+          style={[
+            styles.wrap,
+            {justifyContent: 'center', alignItems: 'center'},
+          ]}>
+          <Image
+            source={not_result_image}
+            style={styles.imageResult}
+            resizeMode="contain"
+          />
+          <Text style={{fontSize: 16}}>Don't have Users with your filter</Text>
+        </View>
+      )} */}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  wrap: {
+    flex: 1,
+    backgroundColor: color.bgWhite,
+  },
   containerAll: {
     width: '100%',
     height: HEIGHT - 49,
@@ -389,5 +411,13 @@ const styles = StyleSheet.create({
     paddingBottom: spacing[4],
     backgroundColor: color.bgWhite,
     borderRadius: spacing[2],
+  },
+  load: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textLoad: {
+    marginTop: 10,
   },
 });
